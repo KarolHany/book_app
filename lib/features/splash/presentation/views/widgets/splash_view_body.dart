6508fd6c02db3567fc33e9cbecb26c0887eef6e3
karol <karolhany09@gmail.com>
2025-06-12 23:00:22 +0300

    refactor initState & navigate to home view

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,5 +1,8 @@
+import 'package:book_app/constants.dart';
 import 'package:book_app/core/utils/assets.dart';
+import 'package:book_app/features/home/presentation/views/home_view.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import 'sliding_text_animation.dart';
 
@@ -16,7 +19,32 @@ class _SplashViewBodyState extends State<SplashViewBody>
   late Animation<Offset> slidingAnimation; // whatever value you want
 
   @override
+  // single responcepility priciple (solid)
   void initState() {
+    super.initState();
+    initStateAnimation();
+    navigateToHome(); 
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose(); // to avoid memory leek
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(AssetsData.logo),
+        SlidingTextAnimation(slidingAnimation: slidingAnimation),
+      ],
+    );
+  }
+
+  void initStateAnimation() {
     animationController =
         AnimationController(vsync: this, duration: const Duration(seconds: 2));
 
@@ -31,24 +59,15 @@ class _SplashViewBodyState extends State<SplashViewBody>
     //     setState(() {});
     //   },
     // );
-    super.initState();
   }
 
-  @override
-  void dispose() {
-    super.dispose();
-    animationController.dispose(); // to avoid memory leek 
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Image.asset(AssetsData.logo),
-        SlidingTextAnimation(slidingAnimation: slidingAnimation),
-      ],
+  void navigateToHome() {
+    Future.delayed(
+      const Duration(seconds: 2),
+      () {
+        Get.to(const HomeView(),
+            transition: Transition.fade, duration: kDurationTransition);
+      },
     );
   }
 }
