e74f0442c938ed8f384d10e4c0c7df38785068be
karol <karolhany09@gmail.com>
2025-07-05 22:04:26 +0300

    add book rating to book details

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:book_app/core/utils/styles.dart';
+import 'package:book_app/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:book_app/features/home/presentation/views/widgets/custom_app_bar_book_details.dart';
 import 'package:book_app/features/home/presentation/views/widgets/custom_book_image_item.dart';
 import 'package:flutter/material.dart';
@@ -9,26 +10,39 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return  Padding(
+    return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         children: [
-           const CustomAppBarBookDetails(),
-           Padding(
-             padding: EdgeInsets.symmetric(horizontal:width * .2 , vertical: 40),
-             child: const CustomBookImageItem(),
-           ),
-           const Text('The Let Them Theory' , style: Styles.textStyle30,),
-            const SizedBox(height: 6,),
-           Opacity(
+          const CustomAppBarBookDetails(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * .2, vertical: 40),
+            child: const CustomBookImageItem(),
+          ),
+          const Text(
+            'The Let Them Theory',
+            style: Styles.textStyle30,
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+          Opacity(
             opacity: .7,
-             child: Text('Mel Robbins' , style: Styles.textStyle18.copyWith(
-              fontStyle: FontStyle.italic,
-             ),),
-           )
+            child: Text(
+              'Mel Robbins',
+              style: Styles.textStyle18.copyWith(
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 18,
+          ),
+          const BookRating(
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
         ],
       ),
     );
   }
 }
-
