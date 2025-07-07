0566ab2ce3c4901fc9cd72839be9b38fea8727a8
karol <karolhany09@gmail.com>
2025-07-08 00:48:46 +0300

    make book details and similar books sections

---
+++ lib/features/home/presentation/views/widgets/book_details_section.dart
@@ -0,0 +1,49 @@
+
+import 'package:book_app/core/utils/styles.dart';
+import 'package:book_app/features/home/presentation/views/widgets/book_rating.dart';
+import 'package:book_app/features/home/presentation/views/widgets/books_buttom_action.dart';
+import 'package:book_app/features/home/presentation/views/widgets/custom_book_image_item.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailsSection extends StatelessWidget {
+  const BookDetailsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: width * .2, vertical: 10),
+          child: const CustomBookImageItem(),
+        ),
+        const Text(
+          'The Let Them Theory',
+          style: Styles.textStyle30,
+        ),
+        const SizedBox(
+          height: 6,
+        ),
+        Opacity(
+          opacity: .7,
+          child: Text(
+            'Mel Robbins',
+            style: Styles.textStyle18.copyWith(
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 18,
+        ),
+        const BookRating(
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+        const SizedBox(
+          height: 37,
+        ),
+        const BooksButtonAction(),
+      ],
+    );
+  }
+}
