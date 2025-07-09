4bc3d97ac1c79be0ab102fc796d83433987d1d53
karol <karolhany09@gmail.com>
2025-07-09 14:57:00 +0300

    update home repo & create failure file

---
+++ lib/features/home/data/repositories/home_repo.dart
@@ -0,0 +1,11 @@
+//? repo mean what the view doing but just an abstract methods not implementation
+
+
+import 'package:book_app/core/errors/failure.dart';
+import 'package:book_app/features/home/data/models/book_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+}
--- lib/features/home/data/repository/home_repo.dart
+++
@@ -1,6 +0,0 @@
-//? repo mean what the view doing but just an abstract methods not implementation
-
-abstract class HomeRepo {
-  fetchFeaturedBooks();
-  fetchBestSellerBooks();
-}
